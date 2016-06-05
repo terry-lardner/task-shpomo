da214000602ccaaaecd77ac51e9582ec3670b6a2
terry-lardner <terryjx@gmail.com>
2016-06-05 23:46:23 +0100

    created modals for teasers

--- js/api-lookup.js
+++ js/api-lookup.js
@@ -1,10 +1,16 @@
 (function() {
 	var $teasers = $('.teasers');
-	var $teasersContainer = $('.teasers').find('.container');
-	var $lookupError = $('.teasers').find('#lookupError');
-	var template = $('.teasers').find('#pixabayTemplate').html();
+	var $teasersContainer = $teasers.find('.container');	 
+	var $lookupError = $teasers.find('#lookupError');
+	var template = $teasers.find('#pixabayTemplate').html();
 	var $teaser;
-	var results = 7;
+	var $modalLink;
+
+	var $modal = $('#modal');
+	var $modalImg = $modal.find('#modalImg');
+	var modalActive = 0;
+
+	var results = 7;	
 
 	function render(obj) {
 		$teasersContainer.append(Mustache.render(template, obj));
@@ -16,7 +22,7 @@
 			url: 'https://pixabay.com/api/?key=2682955-f309748441ab481ea9cde10a6&q=hot+air+balloon&image_type=photo&safesearch=true&per_page=' + results,
 			success: function(data) {
 				if (data) {
-					$.each(data.hits, function(i, result) {
+					$.each(data.hits, function(i, result) {						
 						result.img_url = result.webformatURL;
 						result.page_url = result.pageURL;
 						result.user_page = 'https://pixabay.com/en/users/' + result.user + '-' + result.user_id;
@@ -24,14 +30,15 @@
 					});
 
 					//Cache elements after all teasers are rendered.
-					$teaser = $('.teasers').find('.teaser');
+					$teaser = $teasers.find('.teaser');
+					$modalLink = $teasers.find('.modal-link');
 
 					//Add a gutter to every middle element	
 					for(i=1; i<=$teaser.length; i+=3) {
 						$teaser.eq(i).addClass('gutter');
 					}
 
-					bindEvents();					
+					bindEvents();
 				}
 			},
 			error: function() {
@@ -53,7 +60,41 @@
 			$(this).removeClass('teaser-highlight');
 			$teaserImg.removeClass('teaser-img-border-active');
 		});
+
+		$modalLink.on('click', function() {
+	 		$modal.removeClass('hide');
+	 		$modal.css('top', document.documentElement.scrollTop || document.body.scrollTop)
+	 		$('html').css('overflow', 'hidden');
+	 		$modalImg.attr('src', $(this).data('imgurl'));
+	 		modalActive = 1;
+	 	});
+
+	 	$modal.on('click', function() {
+	 		$modal.addClass('hide');
+	 		$('html').css('overflow', 'auto');
+	 		modalActive = 0;
+	 	});
 	}
 
+	//Resize modal on orientation change
+	$(window).on('orientationchange', function() {
+		if (modalActive == 1) {
+			
+			//repaint background
+
+			setTimeout(function() {
+				var top = document.documentElement.scrollTop || document.body.scrollTop;
+				$modal.offset({top: top});
+
+				// console.log('redrawn')
+				// console.log('----------------------')
+				// console.log('modal: ' + $modal.offset().top)
+				// console.log('document: ' +  document.body.scrollTop)
+				// console.log('----------------------')
+			}, 20);			
+		}
+	});
+
 	pixabayLookup();
+
 }());
